c2271f58eb75b5e23c99d1819035673e58abcf9c
Jonas Claeson <jonas.cl@protonmail.com>
2021-11-20 22:59:21 +0100

    Add an interval timer script

---
+++ timer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TRAIN=$1
+REST=$2
+
+gong () {
+    NBR=$1
+    for i in $(seq 1 $NBR)
+    do
+        play -q gong.mp3 &
+	sleep .3
+    done
+}
+
+while [ true ] ; do
+    echo "active"
+    gong 3 &
+    sleep 10
+    echo -e "rest\n"
+    gong 2 &
+    sleep 5
+done
